b780c6710ea7c370c066a19dddc21a9b150e5485
sai kumar <lmvitservices@gmail.com>
2021-12-31 13:54:49 +0530

    added config file

---
+++ config/index.js
@@ -0,0 +1,7 @@
+/*eslint-disable*/
+export default {
+  RapidInsuranceService: {
+    // baseURL: 'http://localhost:3000',
+    baseURL: '',
+  },
+};
--- helper/api.js
+++ helper/api.js
@@ -1,125 +1,126 @@
 import axios from 'axios';
+import RapidInsuranceService from '../config/index';
 
 export const GetManufacturers = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/vehicle/two-wheeler/manufacturers');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + `/api/vehicle/two-wheeler/manufacturers`);
   return data;
 };
 
 export const GetModels = async (manufacturer_code) => {
   const values = { manufacturer_code };
-  const { data } = await axios.post('http://localhost:3000/api/vehicle/two-wheeler/models', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + `/api/vehicle/two-wheeler/models`, values);
   return data;
 };
 
 export const GetVarients = async (manufacturer_code, model_code) => {
   const values = { manufacturer_code, model_code };
-  const { data } = await axios.post('http://localhost:3000/api/vehicle/two-wheeler/variants', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/vehicle/two-wheeler/variants', values);
   return data;
 };
 
 export const GetPcManufacturers = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/vehicle/private-car/manufacturers');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/vehicle/private-car/manufacturers');
   return data;
 };
 
 export const GetPcModels = async (manufacturer_code) => {
   const values = { manufacturer_code };
-  const { data } = await axios.post('http://localhost:3000/api/vehicle/private-car/models', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/vehicle/private-car/models', values);
   return data;
 };
 
 export const GetPcVarients = async (manufacturer_code, model_code) => {
   const values = { manufacturer_code, model_code };
-  const { data } = await axios.post('http://localhost:3000/api/vehicle/private-car/variants', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/vehicle/private-car/variants', values);
   return data;
 };
 
 /////////////////////////////// RTO Details
 
 export const GetState = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/rto/states');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/states');
   return data;
 };
 
 export const GetLocations = async (state_code) => {
   const values = { state_code };
-  const { data } = await axios.post('http://localhost:3000/api/rto/locations', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/locations', values);
   return data;
 };
 
 export const GetAllLocations = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/rto/all-locations');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/all-locations');
   return data;
 };
 
 export const GetPcState = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/rto/pc-states');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/pc-states');
   return data;
 };
 
 export const GetPcLocations = async (state_code) => {
   const values = { state_code };
-  const { data } = await axios.post('http://localhost:3000/api/rto/pc-locations', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/pc-locations', values);
   return data;
 };
 
 export const GetPcAllLocations = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/rto/pc-all-locations');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/pc-all-locations');
   return data;
 };
 
 export const GetTwState = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/rto/tw-states');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/tw-states');
   return data;
 };
 
 export const GetTwLocations = async (state_code) => {
   const values = { state_code };
-  const { data } = await axios.post('http://localhost:3000/api/rto/tw-locations', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/tw-locations', values);
   return data;
 };
 
 export const GetTwAllLocations = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/rto/pc-all-locations');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/pc-all-locations');
   return data;
 };
 
 export const GetRtoDetails = async (location_code) => {
   const values = { location_code };
-  const { data } = await axios.post('http://localhost:3000/api/rto/rto-details', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/rto/rto-details', values);
   return data;
 };
 
 ////////////////////////////////// Insurance
 
 export const GetInsuranceCompanies = async () => {
-  const { data } = await axios.get('http://localhost:3000/api/insurance_companies');
+  const { data } = await axios.get(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/insurance_companies');
   return data;
 };
 
 export const GetPremiumQuotes = async (values) => {
-  const { data } = await axios.post('http://localhost:3000/api/two-wheeler/premium', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/two-wheeler/premium', values);
   return data;
 };
 
 export const GetPcPremiumQuotes = async (values) => {
   console.log('val', values);
-  const { data } = await axios.post('http://localhost:3000/api/private-car/premium', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/private-car/premium', values);
   return data;
 };
 
 export const PostTwoWheelerBuyNow = async (values) => {
-  const { data } = await axios.post('http://localhost:3000/api/two-wheeler/buy-now', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/two-wheeler/buy-now', values);
   return data;
 };
 
 export const GetOtp = async (values) => {
-  const { data } = await axios.post('http://localhost:3000/api/auth/send-otp', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/auth/send-otp', values);
   return data;
 };
 
 export const VerifyOtp = async (values) => {
-  const { data } = await axios.post('http://localhost:3000/api/auth/verify-otp', values);
+  const { data } = await axios.post(RapidInsuranceService.RapidInsuranceService.baseURL + '/api/auth/verify-otp', values);
   return data;
 };
 
